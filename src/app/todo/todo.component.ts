84d3e7e1fccf485aa7a10a99c7ee70e0396b22aa
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2024-04-25 08:02:07 +0530

    I updated now the when we click on add button it save with time and date this is the update!

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,29 +1,31 @@
 import { Component } from '@angular/core';
 
 interface Todo {
-  text: string; // Define the structure of a to-do item
+  text: string;
+  timestamp: Date;
 }
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
- todos: Todo[] = []; // Initialize an empty list of to-dos
-  newTodo: string = ''; // A variable to hold the text for a new to-do
+  newTodo: string = '';
+  todos: Todo[] = [];
 
-  // Method to add a new to-do
   addTodo() {
     if (this.newTodo.trim() !== '') {
-      const newTodoItem: Todo = { text: this.newTodo.trim() };
-      this.todos.push(newTodoItem); // Add to the list of to-dos
-      this.newTodo = ''; // Clear the input field
+      const newTodoItem: Todo = {
+        text: this.newTodo.trim(),
+        timestamp: new Date(),
+      };
+      this.todos.push(newTodoItem);
+      this.newTodo = ''; 
     }
   }
 
-  // Method to delete a to-do by index
   deleteTodo(index: number) {
-    this.todos.splice(index, 1); // Remove the item from the list
+    this.todos.splice(index, 1);
   }
 }
